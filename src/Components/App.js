6dc630a6d0ba66d143207e1d851bbca04c390f80
bitopan987 <bitopanxon1@gmail.com>
2022-02-07 22:39:31 +0530

    refactor

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="m-10">
+      <header className="text-center mb-10 bg-gray-500 py-6 rounded text-white">
+        <h1 className="text-4xl font-bold mb-5">Code Editor</h1>
+        <p>You can write and Preview HTML and CSS code live!</p>
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
